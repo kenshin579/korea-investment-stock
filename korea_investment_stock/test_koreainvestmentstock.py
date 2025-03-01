94f601370c9706adcb9369438eac3f16ceee7cee
Frank Oh <kenshin579@hotmail.com>
2025-03-01 11:47:16 +0900

    [#10] 시세 API 확인

--- korea_investment_stock/test_koreainvestmentstock.py
+++ korea_investment_stock/test_koreainvestmentstock.py
@@ -1,18 +1,29 @@
 import os
-from unittest import TestCase
-
-from parameterized import parameterized
+from unittest import TestCase, skip
 
 from korea_investment_stock import KoreaInvestment
 from korea_investment_stock.koreainvestmentstock import RETURN_CD
 
+
 class TestKoreaInvestment(TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.kis = KoreaInvestment(
-            api_key=os.getenv('STOCK_API_KOREA_INVESTMENT_API_KEY'),
-            api_secret=os.getenv('STOCK_API_KOREA_INVESTMENT_API_SECRET'),
-            acc_no=os.getenv('STOCK_API_KOREA_INVESTMENT_ACCOUNT_NO')
+        api_key = os.getenv('STOCK_API_KOREA_INVESTMENT_API_KEY')
+        api_secret = os.getenv('STOCK_API_KOREA_INVESTMENT_API_SECRET')
+        acc_no = os.getenv('STOCK_API_KOREA_INVESTMENT_ACCOUNT_NO')
+
+        cls.kis_korea = KoreaInvestment(
+            api_key=api_key,
+            api_secret=api_secret,
+            acc_no=acc_no,
+            exchange='서울'
+        )
+
+        cls.kis_nasdaq = KoreaInvestment(
+            api_key=api_key,
+            api_secret=api_secret,
+            acc_no=acc_no,
+            exchange='나스닥'
         )
 
     def test_stock_info(self):
@@ -23,6 +34,38 @@ class TestKoreaInvestment(TestCase):
 
         for name, ticker, market in test_cases:
             with self.subTest(name=name):
-                resp = self.kis.fetch_stock_info(ticker, market)
+                resp = self.kis_korea.fetch_stock_info(ticker, market)
                 self.assertEqual(resp['rt_cd'], RETURN_CD["SUCCESS"])
                 self.assertEqual(resp['output']['shtn_pdno'], ticker)
+
+    def test_fetch_domestic_price(self):
+        test_cases = [
+            ("samsung", "005930"),
+            ("etf", "294400")
+        ]
+
+        for name, ticker in test_cases:
+            with self.subTest(name=name):
+                resp = self.kis_korea.fetch_price(ticker)
+                print(resp)
+                self.assertEqual(resp['rt_cd'], RETURN_CD["SUCCESS"])
+
+
+    def test_fetch_oversea_price(self):
+        test_cases = [
+            ("apple", "AAPL"),
+            ("tlt", "TLT"),
+            ("jepq", "JEPQ"),
+            ("divo", "DIVO") # todo: 이거 조회가 안됨
+        ]
+
+        for name, ticker in test_cases:
+            with self.subTest(name=name):
+                resp = self.kis_nasdaq.fetch_price(ticker)
+                print(resp)
+                self.assertEqual(resp['rt_cd'], RETURN_CD["SUCCESS"])
+
+    @skip("Skipping test_fetch_kospi_symbols")
+    def test_fetch_kospi_symbols(self):
+        resp = self.kis_korea.fetch_kospi_symbols()
+        print(resp)
