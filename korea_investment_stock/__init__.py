dbd8baff6cb0c394c47dc340ef961683e3f42601
Frank Oh <kenshin579@hotmail.com>
2025-03-16 15:28:53 +0900

    [#16] bumpversion 설정 추가

--- korea_investment_stock/__init__.py
+++ korea_investment_stock/__init__.py
@@ -1,6 +1,6 @@
 """A Python port of Korea-Investment-Stock API"""
 
-__all__ = ("KoreaInvestmentStock",)
+__all__ = ("KoreaInvestment",)
 __version__ = "0.1.10"
 
 from korea_investment_stock.koreainvestmentstock import *
