794819f8a83489ab3753454cf484bb79dfd970b7
Frank Oh <kenshin579@hotmail.com>
2025-03-22 14:11:37 +0900

    [#14] exchange arg 제거 작업

--- examples/nasdaq/nasdaq_limit_buy_order.py
+++ examples/nasdaq/nasdaq_limit_buy_order.py
@@ -15,7 +15,7 @@ broker = korea_investment_stock.KoreaInvestment(
     api_key=key,
     api_secret=secret,
     acc_no=acc_no,
-    exchange='나스닥' # todo: exchange는 제거 예정
+    # exchange='나스닥' # todo: exchange는 제거 예정
 )
 
 resp = broker.create_limit_buy_order(
