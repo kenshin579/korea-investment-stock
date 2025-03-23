ba6db18f573441d1cdd9acde2d974477547c24a4
Frank Oh <kenshin579@hotmail.com>
2025-03-23 18:17:41 +0900

    [#14] export 상수 값

--- korea_investment_stock/__init__.py
+++ korea_investment_stock/__init__.py
@@ -4,5 +4,4 @@ __all__ = ("KoreaInvestment",)
 __version__ = "0.1.12"
 
 from korea_investment_stock.koreainvestmentstock import *
-
-
+from .koreainvestmentstock import MARKET_CODE_MAP, EXCHANGE_CODE_MAP
