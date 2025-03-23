29c44f905097a86c4cef9255e6008efd4e63b580
Frank Oh <kenshin579@hotmail.com>
2025-03-23 18:03:46 +0900

    Bump version: 0.1.11 → 0.1.12

--- korea_investment_stock/__init__.py
+++ korea_investment_stock/__init__.py
@@ -1,7 +1,7 @@
 """A Python port of Korea-Investment-Stock API"""
 
 __all__ = ("KoreaInvestment",)
-__version__ = "0.1.11"
+__version__ = "0.1.12"
 
 from korea_investment_stock.koreainvestmentstock import *
 
